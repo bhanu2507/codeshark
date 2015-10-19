914a8e91ffb56b7b9d46d67a2e0e0c5d79e088ac
Bhanu Mokkala <bhanureddy@gmail.com>
2015-10-19 18:58:10 +0530

    fadein li

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,15 +1,17 @@
 /**
  * Created by bhanu on 15/10/15.
  */
-
+var n = 0;
 $(window).scroll(function(){
-    if ($(window).scrollTop() == $(document).height() - $(window).height()){
-    //    if($(".pagenum:last").val() <= $(".rowcount").val()) {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+     //   if ($(".pagenum:last").val() <= $(".rowcount").val()) {
     //        var pagenum = parseInt($(".pagenum:last").val()) + 1;
-          //  getresult('getresult.php?page='+pagenum);
-    //        alert("end");
+            //  getresult('getresult.php?page='+pagenum);
+    //        alert(pagenum);
+
             getresult();
-     //   }
+            //   }
+    //    }
     }
 });
 
@@ -35,27 +37,53 @@ function getresult() {
 
          });
          */
-
+    //alert($('#loadCount').val());
+    var ustring = 'http://api.indeed.com/ads/apisearch?publisher=3818743618433097&format=json&q=ui,android,ios,bigdata,mobile,cloud&l=&sort=&radius=&st=&jt=&start=' + (parseInt($('#loadCount').val())+24) +'&limit=24&fromage=&filter=&latlong=1&co=in&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2'
+//alert(ustring);
         $.ajax({
-            url: 'http://api.indeed.com/ads/apisearch?publisher=3818743618433097&format=json&q=ui,android,ios,bigdata,mobile,cloud&l=&sort=&radius=&st=&jt=&start=&limit=25&fromage=&filter=&latlong=1&co=in&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2',
+          //  url: 'http://api.indeed.com/ads/apisearch?publisher=3818743618433097&format=json&q=ui,android,ios,bigdata,mobile,cloud&l=&sort=&radius=&st=&jt=&start=&limit=24&fromage=&filter=&latlong=1&co=in&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2',
+            url: ustring,
             dataType: 'json',
+  //          data:  {rowcount:$("#rowcount").val()},
+            beforeSend: function(){
+                $("#loaderDiv").show();
+            },
             success: function(data) {
+                $("#loaderDiv").hide();
                 var items = [];
 
                 $.each(data, function(key, val) {
                         if(key == "results") {
                             $.each(data.results, function (k, v) {
 
-                                items.push('<li><a href="#"><h2>' + v.jobtitle + '</h2><p>' + v.snippet + '</p></a></li>');
+                                //items.push('<li><a href="#"><h2>' + v.jobtitle + '</h2><p>' + v.snippet + '</p></a></li>');
+                               // var litem = $('<li><a href="#"><h2>' + v.jobtitle + '</h2><p>' + v.snippet + '</p></a></li>');
+                               // $('#header ul').append(litem).filter(':last').hide().fadeIn(500);
+                                $('#header ul').append('<li style= display:none;><a href="#"><h2>' + v.jobtitle + '</h2><p>' + v.snippet + '</p></a></li>');
+                               //alert('e');
+                                //litem.fadeIn(2000);
+                                //$('#header ul').append('<li><a href="#"><h2>' + v.jobtitle + '</h2><p>' + v.snippet + '</p></a></li>').children(':last').hide().fadeIn(1000);
                             });
+
                        }
                 });
 
-                $('<ul/>', {
-                    'class': 'interest-list',
-                    html: items.join('')
-                }).appendTo('body');
+                animateLI($('ul#displayJobs li:first'));
+                function animateLI($li) {
+//alert($li.show());
+                    //$li.show();
+                    $li.fadeIn(500).promise().done(function () {
+                        if(!$li.next().length) return;
+                        animateLI($li.next());
+                });
+                }
 
+/*
+                $('<ul/>', {
+                 'class': 'interest-list',
+                    html: items.join('')
+                }).appendTo('body').hide().fadeIn(3000);*/
+                $('#loadCount').val(parseInt($('#loadCount').val()) + 24);
             },
             statusCode: {
                 404: function() {
@@ -64,6 +92,8 @@ function getresult() {
             }
         });
     };
+
+
 //);
 
 //};
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var request = require('request');
 
 var indeed;
 
-request('http://api.indeed.com/ads/apisearch?publisher=3818743618433097&format=json&q=ui,android,ios,bigdata,mobile,cloud&l=&sort=&radius=&st=&jt=&start=&limit=25&fromage=&filter=&latlong=1&co=in&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function (error, response, body) {
+request('http://api.indeed.com/ads/apisearch?publisher=3818743618433097&format=json&q=ui,android,ios,bigdata,mobile,cloud&l=&sort=&radius=&st=&jt=&start=&limit=24&fromage=&filter=&latlong=1&co=in&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     indeed = JSON.parse(body);
     //console.log(body) // Show the HTML for the Google homepage.
